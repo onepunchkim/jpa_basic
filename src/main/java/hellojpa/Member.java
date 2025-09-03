16c586762caa1ab31441bdd5274fa994a963d115
onepunchkim <hyukjin4711@gmail.com>
2025-09-03 18:55:03 +0900

    jpa 1:N mapping

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -13,6 +13,10 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
+    @ManyToOne //ManyToOne 은 mappedBy 제공x (주로 연관관계의 주인이기 때문)
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false) //읽기전용으로 매핑됨
+    private Team team;
+
 //    @Column(name = "TEAM_ID")
 //    private Long teamId;
 
