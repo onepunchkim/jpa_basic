8e2d58e16ffd576f824b88edfda37fd336e16cd4
onepunchkim <hyukjin4711@gmail.com>
2025-09-07 16:56:10 +0900

    jpa join strategy

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -3,9 +3,11 @@ package hellojpa;
 import jakarta.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+//@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) //부모 클래스로 조회 시 자식 클래스를 전부 찾는다.
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE) //@DiscriminatorColumn이 없어도 DTYPE 생성
 @DiscriminatorColumn
-public class Item {
+public abstract class Item {
 
     @Id @GeneratedValue
     private Long id;
